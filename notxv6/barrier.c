312dc2e29682e30808a72abbe7be7a4ae6d6ef9a
Kelo <1376312818@qq.com>
2021-08-11 14:58:17 +0800

    thread lab

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,19 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  int pre_round = round;
+  if (++bstate.nthread == nthread)
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  else
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  bstate.round = pre_round + 1;
+  if (round != pre_round + 1) {
+    round = pre_round + 1;
+    bstate.nthread = 0;
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
